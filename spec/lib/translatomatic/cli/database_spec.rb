41a184405992b2a5d5639587e530a05dc039b18e
Andrew Williams <sobakasu@gmail.com>
2017-12-30 23:54:15 +1030

    Fix database cli spec test

--- spec/lib/translatomatic/cli/database_spec.rb
+++ spec/lib/translatomatic/cli/database_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe Translatomatic::CLI::Database do
 
   before(:each) do
+    skip if database_disabled?
     @cli = Translatomatic::CLI::Database.new
     @cli.options = { database_env: "test" }
     @locale_en = Translatomatic::Model::Locale.find_or_create_by!(language: :en)
@@ -9,11 +10,13 @@ RSpec.describe Translatomatic::CLI::Database do
 
   context :search do
     it "searches for text in the database" do
+      skip if database_disabled?
       text = FactoryBot.create(:text_model, locale: @locale_en)
       @cli.search(text.value)
     end
 
     it "searches for text in the database with specified locale" do
+      skip if database_disabled?
       text = FactoryBot.create(:text_model, locale: @locale_en)
       FactoryBot.create(:text_model, locale: @locale_de, from_text: text)
 
