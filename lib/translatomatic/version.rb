ec3848707700d2834b564c5fa0773b367da009b4
Andrew Williams <sobakasu@gmail.com>
2018-01-06 22:57:20 +1030

    Bump version

--- lib/translatomatic/version.rb
+++ lib/translatomatic/version.rb
@@ -1,6 +1,6 @@
 module Translatomatic
   # @return [String] Gem version
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 
   # @return [String] Homepage
   URL = "https://github.com/smugglys/translatomatic"
