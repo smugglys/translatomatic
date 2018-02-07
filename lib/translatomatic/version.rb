65adc758fa1d51adcab8851aaeefbcb6938cf62e
Andrew Williams <sobakasu@gmail.com>
2018-02-08 08:21:31 +1030

    0.2.0

--- lib/translatomatic/version.rb
+++ lib/translatomatic/version.rb
@@ -1,6 +1,6 @@
 module Translatomatic
   # @return [String] Gem version
-  VERSION = '0.1.3'.freeze
+  VERSION = '0.2.0'.freeze
 
   # @return [String] Homepage
   URL = 'https://github.com/smugglys/translatomatic'.freeze
